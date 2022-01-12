7ca7dbbffc1d31bb5434f64a729dcc2c94103e09
TalgatBatyrov <talgatbatyrov7@gmail.com>
2022-01-12 17:19:26 +0600

    Lets Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello name={'Talgat'} />
+      <Hello name={'Jarkyn'} />
+      <Hello name={'Ayzada'} />
     </div>
   );
 }
 
 export default App;
+
+
+const Hello = ({ name }) => {
+  return (
+    <div>
+      Hello {name}
+    </div>
+  )
+}
